fa77e111b67a105c2edd1df2f2f755bd3b8abe83
Bhuvan Y E <bhuvanye@gmail.com>
2025-08-07 22:38:54 +0530

    fix bug

--- backend/app.js
+++ backend/app.js
@@ -89,12 +89,4 @@ app.delete("/user-places/:id", async (req, res) => {
   res.status(200).json({ userPlaces: updatedUserPlaces });
 });
 
-// 404
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    return next();
-  }
-  res.status(404).json({ message: "404 - Not Found" });
-});
-
 app.listen(3000);
